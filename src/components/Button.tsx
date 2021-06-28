4dae49cc3e89947aaf9da089727a696a741916d7
gippy-marley <gipsongippy99@gmail.com>
2021-06-28 22:36:40 +0530

    📦 NEW: AddTopicModal

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,29 +4,21 @@ import {Box, Text} from './index';
 import {useTheme} from '@shopify/restyle';
 import {Theme} from '../styles/theme';
 
-<<<<<<< HEAD
-type TextButtonProps = {label?: string; textAlign?: string; onPress?: any};
-=======
 type TextButtonProps = {
   label?: string;
   textAlign?: string;
   isLoading?: boolean;
   onPress?: () => void;
 };
->>>>>>> a3a0858c43794b75b365fcd4f336f16593b16b6e
 
 const TextButton = (props: TextButtonProps) => {
   const theme = useTheme<Theme>();
 
   return (
-<<<<<<< HEAD
-    <TouchableOpacity onPress={props.onPress}>
-=======
     <TouchableOpacity
       onPress={() => {
         !props?.onPress || props.onPress();
       }}>
->>>>>>> a3a0858c43794b75b365fcd4f336f16593b16b6e
       <Box
         style={{
           paddingHorizontal: theme.spacing.m,

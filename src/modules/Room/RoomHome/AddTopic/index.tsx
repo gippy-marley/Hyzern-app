9a7b5bd563640bff2f3cd1b057a25046e542ef3a
gippy-marley <gipsongippy99@gmail.com>
2021-06-30 17:51:48 +0530

    📦 NEW: AddTopicModal

--- src/modules/Room/RoomHome/AddTopic/index.tsx
+++ src/modules/Room/RoomHome/AddTopic/index.tsx
@@ -2,38 +2,73 @@ import React, {Component} from 'react';
 import Modal from 'react-native-modal';
 import { StyleSheet, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Box, TextField, TextButton } from '../../../../components';
+import { Alert } from 'react-native';
+import { Keyboard } from 'react-native';
+import { Platform } from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
 
 type props ={
-    toggleModal: ()=> void;
-    modalVisible:any
-}
+  toggleModal: ()=> void;
+  modalVisible: any;
+};
 
-export default class AddTopic extends Component () {
+export default class AddTopic extends Component {
     constructor(props:any){
-        super(props);
-        this.modalVisible;
-        this.toggleModal={};
+      super(props);
+      this.modalVisible;
+      this.toggleModal={};
     }
+
+    //  UNSAFE_componentWillMount(){
+    //   this.keyboardDidShowListener = Keyboard.addListener(
+    //     'keyboardDidShow',
+    //     this._keyboardDidShow,
+    //   );
+    //    this.keyboardDidHideListener = Keyboard.addListener(
+    //     'keyboardDidHide',
+    //     this._keyboardDidHide,
+    //   );
+    // }
+    // componentWillUnmount(){
+    //   this.keyboardDidShowListener.remove();
+    //   this.keyboardDidHideListener.remove();
+    // }
+
+    // _keyboardDidShow=()=>{
+      
+    //   scrollView.scrollTo({y:220, animated:true});
+      
+    // };
+
+    // _keyboardDidHide=()=>{
+     
+    // };
 
     render(){
   return(
-        <KeyboardAvoidingView>
-          <ScrollView>      
-        <Modal isVisible={ this.modalVisible } swipeDirection="down" onSwipeComplete ={()=> this.toggleModal()}
-        onBackButtonPress={()=> this.toggleModal()} style={{margin: 0, padding: 0}}>
+        // <KeyboardAvoidingView style={{flex:1}}
+        // behavior={Platform.OS==='ios'?'padding':''}>
+        //   <ScrollView
+        //   ref={(ref:any) => ( scrollView = ref )} contentContainerStyle={{flex:1}} bounces={false}>
+        <KeyboardAwareScrollView innerRef={(ref) => {this.scroll = ref}}>      
+        <Modal isVisible={ this.props.modalVisible } swipeDirection="down" onSwipeComplete ={()=> this.props.toggleModal()}
+        onBackButtonPress={()=> this.props.toggleModal()} style={{margin: 0, padding: 0}}>
           <Box style={{marginTop:150, width: '100%', height:'100%', margin: 0,
           borderTopLeftRadius: 18, borderTopRightRadius: 18, padding: 10}}
           backgroundColor={'mainBackground'}>     
-            <TextField margin={40} height={110} textAlignVertical={'top'} borderColor={null} placeholder="Topic Name"></TextField>
+            <TextField onFocus={(event:any)=>{this._scrollToInput(React.findNodeHandle(event.target))}} margin={40} height={110} textAlignVertical={'top'} borderColor={null} placeholder="Topic Name"></TextField>
               <Box flexDirection={'row'} justifyContent={'space-between'} style={{marginHorizontal: 50}}>
                 <TextButton label="For" style={[styles.textButton, {backgroundColor: '#423bcf'}]} ></TextButton>
                 <TextButton label="Against" style={[styles.textButton, {backgroundColor: '#f1484b'}]}></TextButton>
               </Box>
-            <TextButton label="Let's do it!" style={[styles.textButton, {backgroundColor: '#01A56D', width: 150, alignSelf: 'center', marginTop: 200}]} ></TextButton>
+            <TextButton label="Let's do it!" style={[styles.textButton,
+              {backgroundColor: '#01A56D', width: 150, alignSelf: 'center', marginTop: 200}]}
+              onPress={()=> Alert.alert('hi')}></TextButton>
           </Box>                
         </Modal>
-        </ScrollView>
-        </KeyboardAvoidingView>
+        {/* </ScrollView>
+        </KeyboardAvoidingView> */}
+        </KeyboardAwareScrollView>
   )
   }
 }
--- src/modules/Room/RoomHome/Start.tsx
+++ src/modules/Room/RoomHome/Start.tsx
@@ -16,24 +16,26 @@ const Start = () =>{
   return(
     <Box marginHorizontal={'3xl'} bottom={200}>
       <ButtonAlt label="Start a talk" textAlign="center" onPress={ ()=> toggleModal()} />
-      {/* <AddTopic toggleModal={toggleModal} modalVisible={modalVisible}></AddTopic> */}
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : ""} style={{flex:1}}>
-        <ScrollView>      
+      <AddTopic toggleModal={toggleModal} modalVisible={modalVisible}></AddTopic>
+{/*          
       <Modal isVisible={ modalVisible } swipeDirection="down" onSwipeComplete ={()=> toggleModal()}
       onBackButtonPress={()=> toggleModal()} style={{margin: 0, padding: 0}}>
-        <Box style={{marginTop:150, width: '100%', height:'100%', margin: 0,
+         <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : ""} style={{flex:1,  margin:0, height:'50%'}}>
+        <ScrollView >  
+        <Box style={{marginTop: 100, width: '100%', height:'100%', bottom: 10, marginBottom: 70,
         borderTopLeftRadius: 18, borderTopRightRadius: 18, padding: 10}}
         backgroundColor={'mainBackground'}>     
-          <TextField margin={40} height={110} textAlignVertical={'top'} borderColor={null} placeholder="Topic Name"></TextField>
+          <TextField margin={40} height={110} textAlignVertical={'top'} borderColor={'transparent'} placeholder="Topic Name"></TextField>
             <Box flexDirection={'row'} justifyContent={'space-between'} style={{marginHorizontal: 50}}>
               <TextButton label="For" style={[styles.textButton, {backgroundColor: '#423bcf'}]} ></TextButton>
               <TextButton label="Against" style={[styles.textButton, {backgroundColor: '#f1484b'}]}></TextButton>
             </Box>
           <TextButton label="Let's do it!" style={[styles.textButton, {backgroundColor: '#01A56D', width: 150, alignSelf: 'center', marginTop: 200}]} ></TextButton>
-        </Box>                
-      </Modal>
-      </ScrollView>
-      </KeyboardAvoidingView>
+        </Box>               
+        </ScrollView>
+      </KeyboardAvoidingView> 
+      </Modal> */}
+      
     </Box>
   )
 }
